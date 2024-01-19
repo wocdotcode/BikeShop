2feea60516cbede52429d42e62c74a7ad023a58e
Wendell Centeno <dellortiz06@gmail.com>
2024-01-19 14:19:07 +0800

    Initial commit

---
+++ BikeShopWebParent/BikeShopBackend/src/main/java/com/bikeshop/admin/BikeShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bikeshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeShopBackendApplication.class, args);
+	}
+
+}
---
+++ BikeShopWebParent/BikeShopFrontend/src/main/java/com/bikeshop/site/BikeShopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bikeshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeShopFrontendApplication.class, args);
+	}
+
+}

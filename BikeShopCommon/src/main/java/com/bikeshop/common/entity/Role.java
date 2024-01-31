3b29ad0e1629cf17c5e7629d271e6a8a0b3d1a65
Wendell Centeno <dellortiz06@gmail.com>
2024-01-31 18:55:35 +0800

    Create UserService and code listAll method then display all users in the users.html

--- BikeShopCommon/src/main/java/com/bikeshop/common/entity/Role.java
+++ BikeShopCommon/src/main/java/com/bikeshop/common/entity/Role.java
@@ -77,8 +77,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "name='" + name + '\'' +
-                '}';
+        return this.name;
     }
 }

9fc382d4423c27f155b1280ce3c28c18593ab64f
Wendell Centeno <dellortiz06@gmail.com>
2024-03-29 16:02:24 +0800

    feat: Update User function

---
+++ BikeShopWebParent/BikeShopBackend/src/main/java/com/bikeshop/admin/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bikeshop.admin.user;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String s) {
+        super(s);
+    }
+}
--- BikeShopWebParent/BikeShopBackend/src/main/java/com/bikeshop/admin/user/UserService.java
+++ BikeShopWebParent/BikeShopBackend/src/main/java/com/bikeshop/admin/user/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -36,13 +37,49 @@ public class UserService {
     }
 
     public void save(User user){
+        boolean isUpdatingUser = (user.getId() != null); //updating mode
+
+        if (isUpdatingUser){
+            User existingUser = userRepo.findById(user.getId()).get();
+
+            if(user.getPassword().isEmpty()){
+                user.setPassword(existingUser.getPassword());
+            } else {
+                encodePassword(user);
+            }
+        }else {
+            encodePassword(user);
+        }
+
         encodePassword(user);
         userRepo.save(user);
     }
 
-    public boolean isEmailUnique(String email){
+    public boolean isEmailUnique(Integer id,String email){
         User userByEmail = userRepo.getUserByEmail(email);
-        return userByEmail == null;
+
+        if (userByEmail == null) return true;
+
+        boolean isCreatingNew = (id == null);
+        if(isCreatingNew){
+            if(userByEmail != null) return false;
+        } else{
+            if(userByEmail.getId() != id){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+
+        try {
+            return userRepo.findById(id).get();
+        } catch(NoSuchElementException ex){
+            throw new UserNotFoundException("Could not find any user with ID " + id);
+        }
 
     }
 
